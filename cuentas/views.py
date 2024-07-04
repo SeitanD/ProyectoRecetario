1983986234c558975ed989d09b0ea71ac72c3419
Sebastian Diaz <sn.diaz@duocuc.cl>
2024-07-03 22:47:55 -0400

    migrar sumativa2 a sumativa3

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
-
     return render(request, 'cuentas/index.html')
 
 def aperitivos(request):
-
     return render(request, 'cuentas/aperitivos.html')
+
+def comida(request):
+    return render(request, 'cuentas/comida.html')
+
+def batidos(request):
+    return render(request, 'cuentas/batidos.html')
+
+def contacto(request):
+    return render(request, 'cuentas/contacto.html')
+
+def login(request):
+    return render(request, 'cuentas/login.html')
+
+def registro(request):
+    return render(request, 'cuentas/registro.html')
+
+def olvido_contraseña(request):
+    return render(request, 'cuentas/olvido-contraseña.html')
